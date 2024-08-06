00c1a41e36b7c185ba4b8cbff8e30712ad6efbc8
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-08-06 12:52:55 +0500

    created a project

---
+++ src/main/java/kz/kalabay/locationsystem/LocationSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.kalabay.locationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/kalabay/locationsystem/LocationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kalabay.locationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
